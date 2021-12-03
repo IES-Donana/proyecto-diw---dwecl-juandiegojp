ee5692d401e1ac025a22e75905e9a1981f818063
juandiegojp <juandiego.jurado@iesdonana.org>
2021-12-03 17:19:39 +0100

    remove js

--- js/js.js
+++ js/js.js
@@ -1,14 +1,23 @@
 const toggleButton = document.getElementById('button-menu');
 const navWrapper = document.getElementById('nav');
+var WrapperRemove = 0;
+
+
+toggleButton.addEventListener('click',() => {
+  navWrapper.classList.toggle('show');
+  if (WrapperRemove == 1 ) {
+    navWrapper.remove();
+  }
+});
 
 toggleButton.addEventListener('click',() => {
   toggleButton.classList.toggle('close');
-  navWrapper.classList.toggle('show');
+  WrapperRemove = 1;
 });
 
 navWrapper.addEventListener('click',e => {
-    if(e.target.id === 'nav'){
-    navWrapper.classList.remove('show');
-    toggleButton.classList.remove('close');
+    if(e.target.id === 'nav') {
+      navWrapper.classList.remove('show');
+      toggleButton.classList.remove('close');
     }
 });
