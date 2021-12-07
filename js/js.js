0f130a209a0937254deb67b643f39b19ca708c69
Juan Diego Jurado <juandiego_jp@hotmail.com>
2021-12-07 03:51:27 +0100

    JavaScript

--- js/js.js
+++ js/js.js
@@ -1,25 +1,22 @@
+/* Menú Hamburguesa */
+
 const toggleButton = document.getElementById('button-menu');
 const navWrapper = document.getElementById('nav');
-var WrapperRemove = 0;
-let nav = document.createElement("nav");
+/* let nav = document.createElement("nav");
 nav.getAttribute("id","nav");
 nav.getAttribute("class","main-nav");
 let div = document.createElement("div");
 div.getAttribute("class", "nav-links");
 div.appendChild();
 nav.appendChild(div);
-
+ */
 
 toggleButton.addEventListener('click',() => {
   navWrapper.classList.toggle('show');
-  if (WrapperRemove == 1 ) {
-    navWrapper.remove();
-  }
 });
 
 toggleButton.addEventListener('click',() => {
   toggleButton.classList.toggle('close');
-  WrapperRemove = 1;
 });
 
 navWrapper.addEventListener('click',e => {
@@ -28,3 +25,34 @@ navWrapper.addEventListener('click',e => {
       toggleButton.classList.remove('close');
     }
 });
+
+
+/* Validación de formulario */
+var username = document.getElementById("username");
+var email = document.getElementById("usermail");
+var comment = document.getElementById("comentario");
+comment.disabled = true;
+
+/* username.onchange = cambioUser; */
+email.onchange = cambioEmail;
+
+if (username.validity && email.validity) {
+  comment.disabled = false;
+}
+
+function cambioEmail(e) {
+  if (email.validity.typeMismatch) {
+    email.style.backgroundColor = "red";
+    alert("¡Se esperaba una dirección de correo electrónico!");
+  } else {
+    email.style.backgroundColor = "#AAEEAA";
+  }
+  ;
+}
+
+email.addEventListener('keyup', function (event) {
+  if (event.getModifierState('Shift') ) {
+    alert("¡Has introducido una mayúscula!");
+  }
+});
+
