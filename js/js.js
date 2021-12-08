d500ded7ceaf7ffd2699703c1a1b153eb06db6c9
Juan Diego Jurado <juandiego_jp@hotmail.com>
2021-12-08 17:00:42 +0100

    Formulario - Menú - Responsive

--- js/js.js
+++ js/js.js
@@ -1,21 +1,44 @@
 /* Menú Hamburguesa */
-
 const toggleButton = document.getElementById('button-menu');
 const navWrapper = document.getElementById('nav');
-/* let nav = document.createElement("nav");
-nav.getAttribute("id","nav");
-nav.getAttribute("class","main-nav");
+
+let nav = document.getElementById("nav");
 let div = document.createElement("div");
-div.getAttribute("class", "nav-links");
-div.appendChild();
+let inicio = document.createElement("a");
+let proyectos = document.createElement("a");
+let cv = document.createElement("a");
+let sobreMi = document.createElement("a");
+let contacto = document.createElement("a");
+
+div.setAttribute("class","nav-links");
+inicio.setAttribute("class", "link-underline nav-item");
+inicio.setAttribute("href", "#inicio");
+proyectos.setAttribute("class", "link-underline nav-item");
+proyectos.setAttribute("href", "#proyectos");
+cv.setAttribute("class", "link-underline nav-item");
+cv.setAttribute("href", "#cv");
+sobreMi.setAttribute("class", "link-underline nav-item");
+sobreMi.setAttribute("href", "#sobre-mi");
+contacto.setAttribute("class", "link-underline nav-item");
+contacto.setAttribute("href", "#contacto");
+
+inicio.innerText = "Inicio";
+proyectos.innerText = "Proyectos";
+cv.innerText = "CV";
+sobreMi.innerText = "Sobre mí";
+contacto.innerText = "Contacto";
+
+div.appendChild(inicio);
+div.appendChild(proyectos);
+div.appendChild(cv);
+div.appendChild(sobreMi);
+div.appendChild(contacto);
 nav.appendChild(div);
- */
+
+
 
 toggleButton.addEventListener('click',() => {
   navWrapper.classList.toggle('show');
-});
-
-toggleButton.addEventListener('click',() => {
   toggleButton.classList.toggle('close');
 });
 
@@ -24,6 +47,7 @@ navWrapper.addEventListener('click',e => {
       navWrapper.classList.remove('show');
       toggleButton.classList.remove('close');
     }
+    navWrapper.remove();
 });
 
 
@@ -35,6 +59,13 @@ comment.disabled = true;
 
 email.onchange = cambioEmail;
 email.onblur = disComment;
+document.querySelectorAll("form input:not(input[type='submit'])").forEach((e) => 
+                          { e.addEventListener("focus", PalabrasRadiantes, false); }); 
+
+function PalabrasRadiantes(e) {
+  var el = e.target;
+  el.style.borderImage = "linear-gradient(#f6b73c, #4d9f0c) 1";
+}
 
 function disComment(e) {
   if (username.validity.valid && /@/.test(email.value)) {
@@ -53,10 +84,15 @@ function cambioEmail(e) {
   }
 }
 
-
 email.addEventListener('keyup', function (event) {
   if (event.getModifierState('Shift') ) {
     alert("¡Has introducido una mayúscula!");
   }
 });
 
+function validar() {
+  if (username.validity.valid && /@/.test(email.value)) {
+    alert("El formulario está correctamente validado");
+  } else {
+    alert("El formulario no está correctamente validado");
+}};
