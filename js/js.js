72a60adcf2439ae21cd4acf5f7157bf173dda0a1
Juan Diego <78365272+juandiegojp@users.noreply.github.com>
2021-12-10 01:12:28 +0100

    JS - Final

--- js/js.js
+++ js/js.js
@@ -1,27 +1,66 @@
 /* Menú Hamburguesa */
+var toggleButton = document.getElementById('button-menu');
+let nav = document.getElementById("nav");
+let div;
+crearMenu();
 
-const toggleButton = document.getElementById('button-menu');
-const navWrapper = document.getElementById('nav');
-/* let nav = document.createElement("nav");
-nav.getAttribute("id","nav");
-nav.getAttribute("class","main-nav");
-let div = document.createElement("div");
-div.getAttribute("class", "nav-links");
-div.appendChild();
-nav.appendChild(div);
- */
+function crearMenu() {
+  div = document.createElement("div");
+  let inicio = document.createElement("a");
+  let proyectos = document.createElement("a");
+  let cv = document.createElement("a");
+  let sobreMi = document.createElement("a");
+  let contacto = document.createElement("a");
+  
+  div.setAttribute("class","nav-links");
+  inicio.setAttribute("class", "link-underline nav-item");
+  inicio.setAttribute("href", "#inicio");
+  proyectos.setAttribute("class", "link-underline nav-item");
+  proyectos.setAttribute("href", "#proyectos");
+  cv.setAttribute("class", "link-underline nav-item");
+  cv.setAttribute("href", "#cv");
+  sobreMi.setAttribute("class", "link-underline nav-item");
+  sobreMi.setAttribute("href", "#sobre-mi");
+  contacto.setAttribute("class", "link-underline nav-item");
+  contacto.setAttribute("href", "#contacto");
+  
+  inicio.innerText = "Inicio";
+  proyectos.innerText = "Proyectos";
+  cv.innerText = "CV";
+  sobreMi.innerText = "Sobre mí";
+  contacto.innerText = "Contacto";
 
+  div.appendChild(inicio);
+  div.appendChild(proyectos);
+  div.appendChild(cv);
+  div.appendChild(sobreMi);
+  div.appendChild(contacto);
+  nav.appendChild(div);
+}
+
+var x=0;
 toggleButton.addEventListener('click',() => {
-  navWrapper.classList.toggle('show');
+  if (nav.hasChildNodes()) {
+    nav.classList.toggle('show');
+    toggleButton.classList.toggle('close');
+    x++;
+    console.log(x);
+    if (x==2) {
+      div.remove();
+      x=0;
+    }
+  } else {
+    //console.log(nav.hasChildNodes());
+    crearMenu();
+    nav.classList.toggle('show');
+    toggleButton.classList.toggle('close');
+    x++;
+  }
 });
 
-toggleButton.addEventListener('click',() => {
-  toggleButton.classList.toggle('close');
-});
-
-navWrapper.addEventListener('click',e => {
+nav.addEventListener('click',e => {
     if(e.target.id === 'nav') {
-      navWrapper.classList.remove('show');
+      nav.classList.remove('show');
       toggleButton.classList.remove('close');
     }
 });
@@ -29,24 +68,36 @@ navWrapper.addEventListener('click',e => {
 
 /* Validación de formulario */
 var username = document.getElementById("username");
+var apellido = document.getElementById("apellido");
 var email = document.getElementById("usermail");
 var comment = document.getElementById("comentario");
 comment.disabled = true;
 
-/* username.onchange = cambioUser; */
 email.onchange = cambioEmail;
+email.onblur = disComment;
+document.querySelectorAll("form input:not(input[type='submit'])").forEach((e) => 
+                          { e.addEventListener("focus", PalabrasRadiantes, false); }); 
 
-if (username.validity && email.validity) {
-  comment.disabled = false;
+function PalabrasRadiantes(e) {
+  var el = e.target;
+  el.style.borderImage = "linear-gradient(#f6b73c, #4d9f0c) 1";
+}
+
+function disComment(e) {
+  if (username.validity.valid && /@/.test(email.value)) {
+    comment.disabled = false;
+  } else {
+    comment.disabled = true;
+  }
 }
 
 function cambioEmail(e) {
-  if (email.validity.typeMismatch) {
+  if (!/@/.test(email.value)) {
     email.style.backgroundColor = "red";
     alert("¡Se esperaba una dirección de correo electrónico!");
   } else {
     email.style.backgroundColor = "#AAEEAA";
-  };
+  }
 }
 
 email.addEventListener('keyup', function (event) {
@@ -55,3 +106,14 @@ email.addEventListener('keyup', function (event) {
   }
 });
 
+function validar(e) {
+  if (username.validity.valid && apellido.validity.valid && /@/.test(email.value)) {
+    if(!confirm("El formulario será enviado, ¿estás seguro de los campos rellenados?")) {
+      e.preventDefault();
+      console.log("Rechazado.");
+    } else {
+      console.log("Formulario validado y enviado.");
+    }
+  } else {
+    alert("El formulario no está correctamente validado");
+}};
